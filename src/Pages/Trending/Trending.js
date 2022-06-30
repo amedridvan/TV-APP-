ae656b99a1792959afb30a8661089482a1db34eb
amedridvan <ahmedridvan1@gmail.com>
2022-06-30 22:58:08 +0300

    code done

---
+++ src/Pages/Trending/Trending.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import "./Trending.css";
+import { useEffect, useState } from "react";
+import SingleContent from "../../components/SingleContent/SingleContent";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+
+const Trending = () => {
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const key1="4e3cbc9d9633644d7a56c15cb1919196"
+  const fetchTrending = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${key1}&page=${page}`
+    );
+     
+    setContent(data.results);
+    print(data)
+  };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    fetchTrending();
+    // eslint-disable-next-line
+  }, [page]);
+
+  return (
+    <div>
+      <span className="pageTitle">Trending Today</span>
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type={c.media_type}
+              vote_average={c.vote_average}
+            />
+          ))}
+      </div>
+      <CustomPagination setPage={setPage} />
+    </div>
+  );
+};
+
+export default Trending;
